7f8b0a0d6f4a097f0bbe695fe5a581f8287bb87d
brunoBrian <bruninho_brian@hotmail.com>
2016-07-22 16:59:16 -0300

    Add files via upload

---
+++ Layout_Quatro_Rodas/js/script.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function() { 
+    $(".menuItensCarros").click(function(event){
+			$(".menuFerramentasCarro").slideToggle("slow");       	
+    }); 
+	
+});
+$(document).ready(function() { 
+    $(".menuItensTeste").click(function(){
+			$(".menuFerramentasTeste").slideToggle("slow");        	
+    }); 
+	
+});
